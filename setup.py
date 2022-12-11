4b68e59e18b525d2c2cb53ef2d97c44dba272fe8
Dryhb <66953926+Dryhb@users.noreply.github.com>
2022-12-11 18:41:11 +0100

    Final update finalt working I think

--- setup.py
+++ setup.py
@@ -7,22 +7,17 @@ classifiers = [
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
-# GitHub\DryhbSupport\
-# GitHub\DryhbSupport\
+
 setup(
   name='DryhbSupport',
-  version='0.0.3',
+  version='0.0.7',
   description='Just stuff I recode all the time',
-  long_description=open("README.txt").read()+'\n\n'+open('CHANGELOG.txt').read(),
-  long_description_content_type='text/markdown',
   url='https://github.com/Dryhb/DryhbSupport',
   author='Dryhb',
   author_email='dryhb.pseudotooshort@gmail.com',
   license='MIT',
   classifiers=classifiers,
   keywords='timethat factorial combination', 
-  packages=find_packages(),
-  include_package_data=True,
-  package_data={'DryhbSupport': ['README.txt','CHANGELOG.txt',"License.txt"]},
-  install_requires=[''] 
+  packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+  install_requires=['']
 )
